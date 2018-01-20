a1133ab5fc99447db131c5d298d13f4fa30bfaf9
Hank <l945457758@gmail.com>
2018-01-20 11:11:22 +0800

    ProductDao的实现

---
+++ src/service/ProductService.java
@@ -0,0 +1,34 @@
+package service;
+
+import dao.ProductDao;
+import page.PageBean;
+import product.Product;
+
+public class ProductService {
+    ProductDao productDao = new ProductDao();
+
+    public void add(Product product){
+        productDao.add(product);
+    }
+
+    public void delete(String id){
+        productDao.delete(id);
+    }
+
+    public void edit(Product product){
+        productDao.edit(product);
+    }
+
+    public Product find(String id){
+        return productDao.find(id);
+    }
+
+    public PageBean<Product> showAll(int pageCode, int pageRecord){
+        return productDao.showAll(pageCode, pageRecord);
+    }
+
+    public PageBean<Product> query(Product product, int pageCode, int pageRecord){
+        return productDao.query(product, pageCode, pageRecord);
+    }
+
+}
