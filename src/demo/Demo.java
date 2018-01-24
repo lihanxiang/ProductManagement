69df55ceb700648a8fa4897aac1f05c6e6c0fb7d
Hank <l945457758@gmail.com>
2018-01-24 08:17:58 +0800

    Demo

---
+++ src/demo/Demo.java
@@ -0,0 +1,22 @@
+package demo;
+
+import cn.itcast.commons.CommonUtils;
+import product.Product;
+import service.ProductService;
+
+public class Demo {
+    public static void main(String[] args){
+        ProductService productService = new ProductService();
+        Product product = new Product();
+        for (int i = 1; i < 10; i++) {
+            product.setId(CommonUtils.uuid());
+            product.setBarcode("123456" + i);
+            product.setName("T-shirt" + i);
+            product.setUnits("piece");
+            product.setPurchasePrice("100" + i);
+            product.setSalePrice("150" + i);
+            product.setInventory("50");
+            productService.add(product);
+        }
+    }
+}
