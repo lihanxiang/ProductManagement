af901045200997faec35f9bbaf2fb22815196cf9
Hank <l945457758@gmail.com>
2018-02-22 10:19:39 +0800

    Update

--- src/servlet/ProductServlet.java
+++ src/servlet/ProductServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
+
 
 /*ProductServlet是为了实现页面间的连接
   和通信而设计的，实现了ProductService
